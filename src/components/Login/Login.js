d752936af2aaef45c8d888ee4d2d395a49df4cef
Gabriel Golzman <ggolzman@primerosystems.com>
2023-05-05 11:18:00 -0300

    Added bootstrap and login mockup

---
+++ src/components/Login/Login.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+import "./Login.css";
+
+const Login = () => {
+  return (
+    <div className="login-container">
+      <div className="login-box">
+        <h4>¡Bienvenidos a Book Champions!</h4>
+        <div className="input-container">
+          <input
+            className="input-control"
+            placeholder="Email"
+            type="email"
+          />
+        </div>
+        <div className="input-container">
+          <input
+            className="input-control"
+            
+            placeholder="Password"
+            type="password"
+          />
+        </div>
+        <button  className="signin-button" type="button">
+          Iniciar sesión
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
